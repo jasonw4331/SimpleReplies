206aef6d761f147aa23271042133eb9a3958b3fb
jasonwynn10 <jasonwynn10@me.com>
2021-11-21 12:52:18 -0500

    Missed this before...

--- src/jasonwynn10/SimpleReplies/Main.php
+++ src/jasonwynn10/SimpleReplies/Main.php
@@ -22,7 +22,7 @@ class Main extends PluginBase {
 	public function onEnable() : void {
 		$commandMap = $this->getServer()->getCommandMap();
 		$commandMap->unregister($commandMap->getCommand('tell') ?? throw new AssumptionFailedError('Tell command does not exist'));
-		$commandMap->register("pocketmine", new class("tell") extends VanillaCommand {
+		$commandMap->register("pocketmine", new class("tell", $this) extends VanillaCommand {
 			private Main $plugin;
 			public function __construct(string $name, Main $plugin){
 				$this->plugin = $plugin;
